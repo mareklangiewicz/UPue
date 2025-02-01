9cddb876dd55646c068ec84f81a680c50b7a5c14
Marek Langiewicz <marek.langiewicz@gmail.com>
2025-02-01 20:44:59 +0100

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ defaultBuildTemplateForRootProject(
     name = "UPue",
     description = "Micro Multiplatform Reactive Library.",
     githubUrl = "https://github.com/mareklangiewicz/UPue",
-    version = Ver(0, 0, 20),
+    version = Ver(0, 0, 21),
     // https://s01.oss.sonatype.org/content/repositories/releases/pl/mareklangiewicz/upue/
     // https://github.com/mareklangiewicz/UPue/releases
     settings = LibSettings(
